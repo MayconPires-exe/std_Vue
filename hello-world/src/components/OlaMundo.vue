edf4e3fc6c9067b1237576ba2f7063ac0e57285d
Maycon Pires <piresmaycon4@gmail.com>
2022-05-13 08:04:20 -0300

    Iniciação de projeto

---
+++ hello-world/src/components/OlaMundo.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>olá mundo</h1>
+  <h2>Primeiro projeto Vue 3</h2>
+</template>
+
+<script lang="ts">
+
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    name: "OlaMundo"
+  })
+
+</script>
+
+<style>
+  h1, h2 {
+    text-align: center;
+    color: #c56363;
+  }
+</style>
