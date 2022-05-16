2b52599b69679f0debf6f6b3c07cc902cc5c289d
Maycon Pires <maycon.pires@tmh.com.br>
2022-05-16 17:43:54 -0300

    Termina depois

--- hello-world/alura-tracker/src/App.vue
+++ hello-world/alura-tracker/src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <main class="columns is-gapless is-multiline">
-
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarters">
+      <Formulario />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/FormularioLateral.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    BarraLateral,
+    Formulario
+}
 });
 </script>
 
